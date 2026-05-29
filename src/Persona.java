106f71e624449fb0bf608be59edc47ce2e352c3e
Rey Enmanuel Marte Picherdo <email>
2026-05-29 01:04:31 -0400

    Estructuracion de la clase Persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,22 @@
 public class Persona {
+    private String Nombre;
+    private String Apellido;
+    private String Edad;
+    private String Nacionalidad;
+
+    public Persona() {
+        Nombre = "";
+        Apellido = "";
+        Edad = "";
+        Nacionalidad = "";
+    }
+
+    public Persona(String Nombre, String Apellido, String Edad, String Nacionalidad) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Edad = Edad;
+        this.Nacionalidad = Nacionalidad;
+    }
+
     
 }
