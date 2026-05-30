9c216255905c59d6c636db70df01fe10fa7525f6
Rey Enmanuel Marte Picherdo <email>
2026-05-29 22:46:38 -0400

    Creacion y Estructuracion de la clase Estudiantes

---
+++ src/Estudiantes.java
@@ -0,0 +1,70 @@
+public class Estudiantes extends Persona {
+    private String Matricula;
+    private String Carrera;
+    private String Estado;
+    private String FechaInscripcion;
+
+    public Estudiantes() {
+        super();
+        Matricula = ""; 
+        Carrera = "";
+        Estado = "";
+        FechaInscripcion = "";
+
+    }
+
+    public Estudiantes(String Matricula, String Carrera, String Estado, String FechaInscripcion, String Nombre, String Apellido, String Edad, String Nacionalidad){
+        super(Nombre, Apellido, Edad, Nacionalidad);
+        this.Matricula = Matricula;
+        this.Carrera = Carrera;
+        this.Estado = Estado;
+        this.FechaInscripcion = FechaInscripcion;
+    }
+
+    public void setMatricula(String matricula) {
+        this.Matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setCarrera(String carrera) {
+        this.Carrera = carrera;
+    }
+
+    public String getCarrera() {
+        return Carrera;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setFechaInscripcion(String fecha) {
+        this.FechaInscripcion = fecha;
+    }
+
+    public String getFechaInscripcion() {
+        return FechaInscripcion;
+    }
+
+    public void MostrarEstudiante() {
+        System.out.println("------------------------------------------");
+        System.out.println("Nombre: " + getNombre() + " " + getApellido());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Nacionalidad: " + getNacionalidad());
+        System.out.println("Matricula: " + getMatricula());
+        System.out.println("Carrera: " + getCarrera());
+        System.out.println("Estado: " + getEstado());
+        System.out.println("Fecha de Inscripcion: " + getFechaInscripcion());
+        System.out.println("Materias: " /*+ MostrarMaterias()*/);
+        System.out.println("------------------------------------------");
+    }
+
+    
+}
